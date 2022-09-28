d328645b1ec2bb005a4daacb8cc988f99439e808
Toby Rushton <tobyjrushton@icloud.com>
2022-09-28 15:14:05 +0100

    apollo client configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
