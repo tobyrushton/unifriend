98b1278173e6c3d5ebe59df6f4eb63b3ab0800db
Toby Rushton <tobyjrushton@icloud.com>
2022-11-03 12:48:06 +0000

    index.tsx cleared

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { UserObject } from '../types'
-import { useCreateUser, useSignUp } from '../hooks'
 
 const Home: React.FC = () => {
-    // const createUserInformation: UserObject = useMemo(
-    //     () => ({
-    //         firstName: 'Olivia',
-    //         lastName: 'Rushton',
-    //         university: 'Sheffield University',
-    //         course: 'Financial Mathematics',
-    //         birthday: '27102002',
-    //         username: 'oliviarushton4',
-    //         bio: '',
-    //     }),
-    //     []
-    // )
-
-    // const { loading, error, success } = useCreateUser(createUserInformation)
-
-    const { data, error, loading } = useSignUp('113256', 'txbyplayz@gmail.com')
-    useEffect(() => {
-        console.log('loading: ', loading)
-    }, [loading])
-    useEffect(() => {
-        console.log('error: ', error)
-    }, [error])
-    useEffect(() => {
-        console.log('data: ', data)
-    }, [data])
-
     return (
         <div className={styles.container}>
             <Head>
