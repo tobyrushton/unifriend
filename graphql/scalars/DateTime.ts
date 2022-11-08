b574da845a8fa0f3cf37151b2e9990f3f27e9c2e
Toby Rushton <tobyjrushton@icloud.com>
2022-11-08 13:08:19 +0000

    comments added to DateTime.ts

--- graphql/scalars/DateTime.ts
+++ graphql/scalars/DateTime.ts
@@ -1,14 +1,15 @@
 import { scalarType } from 'nexus'
 import { Kind } from 'graphql'
 
+// creates DateTime type
 export const DateTime = scalarType({
     name: 'DateTime',
     asNexusMethod: 'dateTime',
     description: 'DateTime custom scalar type.',
     parseValue: value => value,
-    serialize: value => new Date((value as number) * 1000).toISOString(),
+    serialize: value => new Date((value as number) * 1000).toISOString(), // creates ISO string of time
     parseLiteral: ast =>
         ast.kind === Kind.INT
-            ? new Date(parseInt(ast.value, 10) * 1000).toISOString()
+            ? new Date(parseInt(ast.value, 10) * 1000).toISOString() // creates ISO string of time
             : null,
 })
