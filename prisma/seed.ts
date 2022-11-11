b86853345db93914075e417c45da2fa317b29c5e
Toby Rushton <tobyjrushton@icloud.com>
2022-11-11 21:00:13 +0000

    prisma seed file errors fixed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,39 +2,41 @@
 
 import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// // creates fake data in the database in order for testing purposes
-// async function main() {
-//     await prisma.users.create({
-//         data: {
-//             firstName: 'Toby',
-//             lastName: 'Rushton',
-//             university: 'Oxford University',
-//             course: 'Computer Science',
-//             username: 'tobyrushton',
-//             birthday: '12062005',
-//             bio: '',
-//         },
-//     })
-//     await prisma.users.create({
-//         data: {
-//             firstName: 'Olivia',
-//             lastName: 'Rushton',
-//             university: 'Sheffield University',
-//             course: 'Financial Mathematics',
-//             username: 'oliviarushton',
-//             birthday: '27072002',
-//             bio: '',
-//         },
-//     })
-// }
+// creates fake data in the database in order for testing purposes
+async function main() {
+    await prisma.users.create({
+        data: {
+            firstName: 'Toby',
+            lastName: 'Rushton',
+            university: 'Oxford University',
+            course: 'Computer Science',
+            username: 'tobyrushton',
+            birthday: '12062005',
+            bio: '',
+            email: 'temp@email.com'
+        },
+    })
+    await prisma.users.create({
+        data: {
+            firstName: 'Olivia',
+            lastName: 'Rushton',
+            university: 'Sheffield University',
+            course: 'Financial Mathematics',
+            username: 'oliviarushton',
+            birthday: '27072002',
+            bio: '',
+            email: 'temp2@email.com'
+        },
+    })
+}
 
-// main()
-//     .catch(e => {
-//         console.error(e)
-//         process.exit(1)
-//     })
-//     .finally(async () => {
-//         await prisma.$disconnect()
-//     })
+main()
+    .catch(e => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
