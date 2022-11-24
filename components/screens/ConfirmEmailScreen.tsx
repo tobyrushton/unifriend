d37f1993981c1c1c8f7e7b3e69f7b5274952b1ec
Toby Rushton <tobyjrushton@icloud.com>
2022-11-24 17:29:09 +0000

    styling update

--- components/screens/ConfirmEmailScreen.tsx
+++ components/screens/ConfirmEmailScreen.tsx
@@ -4,8 +4,10 @@ import { Text } from '../ui'
 export const ConfirmEmailScreen: FC = () => {
     return (
         <>
-            <Text header>Please confirm your email</Text>
-            <Text>
+            <Text header textAlign="center">
+                Please confirm your email
+            </Text>
+            <Text textAlign="center" style={{ marginTop: '10%' }}>
                 If the email does not appear in your inbox, please check your
                 junk folder.
             </Text>
