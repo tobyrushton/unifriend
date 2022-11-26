8ba05549bd5760ee39d2eeae26a968519c4a6e69
Toby Rushton <tobyjrushton@icloud.com>
2022-11-26 17:29:58 +0000

    Lint text tests

--- __tests__/components/UI/Text.test.tsx
+++ __tests__/components/UI/Text.test.tsx
@@ -2,21 +2,21 @@ import { render, fireEvent } from '@testing-library/react'
 import { Text } from '../../../components'
 
 describe('Text component tests', () => {
-
     it('renders text inside', () => {
-        const { queryByText } = render(
-            <Text> Hello World! </Text>
-        )
+        const { queryByText } = render(<Text> Hello World! </Text>)
         expect(queryByText(/^Hello World!$/i)).toBeTruthy()
     })
 
     it('click property works', () => {
-        let clicked:boolean = false
-        const onClick = ():void => {
+        let clicked = false
+        const onClick = (): void => {
             clicked = true
         }
         const { getByText } = render(
-            <Text clickable onClick={onClick}> Hello World! </Text>
+            <Text clickable onClick={onClick}>
+                {' '}
+                Hello World!{' '}
+            </Text>
         )
         fireEvent.click(getByText(/^Hello World!$/i))
         expect(clicked).toBeTruthy()
