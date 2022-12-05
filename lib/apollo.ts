0617fe2d57d07e7320b0fb10401684a98a85c3de
Toby Rushton <tobyjrushton@icloud.com>
2022-12-05 21:49:54 +0000

    apollo client url changed

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'https://famous-bombolone-2a6fd0.netlify.app/api/graphql',
     cache: new InMemoryCache(),
 })
 
