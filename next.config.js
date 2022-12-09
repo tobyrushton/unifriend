8968d73a8b99088bd3be1936c2660cd54cd32dff
Toby Rushton <tobyjrushton@icloud.com>
2022-12-09 17:06:30 +0000

    image upload functions added

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   experimental:{appDir: true},
   optimizeFonts: false,
+  images: {
+    remotePatterns: [{
+      protocol:'https',
+      hostname: process.env.NEXT_PUBLIC_SUPABASE_URL.split('//')[1]
+    }],
+    minimumCacheTTL: 0
+  }
 }
 
 module.exports = nextConfig
